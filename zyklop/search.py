65ba15c53dde538192f72405243d317346460ed2
Roman Joost <roman@bromeco.de>
2011-02-10 13:52:15 +1000

    Added small test to find the instance pattern

--- zyklop/search.py
+++ zyklop/search.py
@@ -3,7 +3,47 @@ import re
 import os.path
 
 
-MAXDEPTH = 4
+class Search(object):
+
+    maxdepth = 4
+
+    def __init__(self, top, match):
+        self.top = top
+        self.match = match
+
+    def find(self, children, visited=None, level=0):
+        """ BFS to find a zope sandbox."""
+        if level == self.maxdepth:
+            return
+
+        if visited is None:
+            visited = []
+        else:
+            for c in visited:
+                children += self.get_children(c)
+
+        while children:
+            child = children.pop()
+            if child in visited:
+                continue
+            if child.endswith('bin/instance'):
+                return (child, level)
+            visited.append(child)
+
+        level += 1
+        return self.find(children, visited, level=level)
+
+    def get_children(self, abspath):
+        if not abspath.startswith('/'):
+            raise ValueError(
+                "abspath parameter needs to be an absolute path: {0}".format(
+                    abspath))
+        return self._get_children_helper(abspath)
+
+    def _get_children_helper(self, abspath):
+        """ Helper function which returns a list of children. """
+        raise NotImplemented("Must be implemented in sublcasses.")
+
 
 
 def search_zope2_buildout():
--- zyklop/tests/test_search.py
+++ zyklop/tests/test_search.py
@@ -1,9 +1,34 @@
-import zyklop.search
+import os
+import os.path
+import shutil
+import tempfile
 import unittest
+import zyklop.search
 
 
 class TestSearch(unittest.TestCase):
     """ Basic test to test the BFS and DFS searches """
 
+    def setUp(self):
+        self.tempdir = tempfile.mkdtemp()
+        src = os.path.join(os.path.dirname(__file__), 'testdata')
+        for item in os.listdir(src):
+            shutil.copytree(os.path.join(src, item),
+                            os.path.join(self.tempdir, item))
+        self.addCleanup(self.cleanTempDir, self.tempdir)
+
+    def cleanTempDir(self, tempdir):
+        shutil.rmtree(tempdir)
+
+    def children_helper(self, path):
+        if os.path.isdir(path):
+            return [os.path.join(path, x) for x in os.listdir(path)]
+        return []
+
     def test_base(self):
-        self.assertEqual(6, 6)
+        search = zyklop.search.Search(self.tempdir, 'bin/instance')
+        # we'll use os.listdir to get the children
+        search._get_children_helper = self.children_helper
+        child, level = search.find(self.children_helper(search.top))
+        self.assertTrue(child is not None)
+        self.assertEquals(level, 2)
