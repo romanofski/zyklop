0279d9586be227cef67c1c6b3ac591095b422162
Roman Joost <roman@bromeco.de>
2011-02-10 11:01:50 +1000

    Bootstrapping tests; now using py.test

---
+++ zyklop/tests/test_search.py
@@ -0,0 +1,9 @@
+import zyklop.search
+import unittest
+
+
+class TestSearch(unittest.TestCase):
+    """ Basic test to test the BFS and DFS searches """
+
+    def test_base(self):
+        self.assertEqual(6, 6)
