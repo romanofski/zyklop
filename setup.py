19149936a9a84f48c4072c9809e06221c89d436b
Roman Joost <roman@bromeco.de>
2011-02-10 15:16:01 +1000

    Hooked up the search function

--- setup.py
+++ setup.py
@@ -2,27 +2,33 @@ from setuptools import setup, find_packages
 
 version = '0.1'
 
-setup(name='zyklop',
-      version=version,
-      description="Administrate servers",
-      long_description="",
-      classifiers=[
-          "Programming Language :: Python 3",
-          "Topic :: Software Development :: Libraries :: Python Modules",
-      ],
-      keywords='server',
-      author='Roman Joost',
-      author_email='roman@bromeco.de',
-      url='',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'fabric',
-      ],
-      extras_require=dict(
-          test=['mocker',]
-      ),
-     )
+setup(
+    name='zyklop',
+    version=version,
+    description="Administrate servers",
+    long_description="",
+    classifiers=[
+        "Programming Language :: Python 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='server',
+    author='Roman Joost',
+    author_email='roman@bromeco.de',
+    url='',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'fabric',
+    ],
+    extras_require=dict(
+        test=['mocker',]
+    ),
+    entry_points = {
+        'console_scripts': [
+            'search = zyklop.search:search_for_zope',
+        ]
+    }
+)
