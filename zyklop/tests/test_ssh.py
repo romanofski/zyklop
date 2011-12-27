d720204389091189de5edb203c3b4e007a617e9f
Roman Joost <roman@bromeco.de>
2011-12-27 18:32:28 +1000

    Reuse an sshrsync instance

--- zyklop/tests/test_ssh.py
+++ zyklop/tests/test_ssh.py
@@ -6,41 +6,41 @@ import zyklop.tests.base
 
 class TestSSHRSync(zyklop.tests.base.BaseDirectoryTestCase):
 
-    def test_get_hashes_for(self):
-        rsync = zyklop.ssh.SSHRsync('dummy', 'localhost')
+    def setUp(self):
+        super(TestSSHRSync, self).setUp()
+        self.rsync = zyklop.ssh.SSHRsync('localhost', 22)
 
-        hashes = rsync.get_hashes_for(
+    def test_get_hashes_for(self):
+        hashes = self.rsync.get_hashes_for(
             os.path.join(self.tempdir, 'folder1', 'etc', 'file1.txt'))
         self.assertTrue(hashes)
 
-        self.assertRaises(IOError, rsync.get_hashes_for,
+        self.assertRaises(IOError, self.rsync.get_hashes_for,
                           os.path.join(self.tempdir, 'folder1'))
 
     def test_get_type(self):
-        rsync = zyklop.ssh.SSHRsync('localhost', 22)
-        is_folder = rsync.get_type(os.path.join(self.tempdir, 'folder1'))
+        is_folder = self.rsync.get_type(os.path.join(self.tempdir,
+                                                     'folder1'))
         self.assertEquals(zyklop.ssh.DIRECTORY, is_folder)
 
-        is_file = rsync.get_type(os.path.join(self.tempdir,
-                                                'folder1', 'etc',
-                                                'file1.txt'))
+        is_file = self.rsync.get_type(os.path.join(self.tempdir,
+                                                   'folder1', 'etc',
+                                                   'file1.txt'))
         self.assertEquals(zyklop.ssh.FILE, is_file)
 
     def test_sync_file(self):
-        rsync = zyklop.ssh.SSHRsync('localhost', 22)
         localtemp = tempfile.mkdtemp()
         self.addCleanup(self.cleanTempDir, localtemp)
 
         # given a folder an IOError is raised
-        self.assertRaises(IOError, rsync.sync_file, self.tempdir,
+        self.assertRaises(IOError, self.rsync.sync_file, self.tempdir,
                           'dummy')
 
         # normal copy
         remotepath = os.path.join(
             self.tempdir, 'folder1', 'etc', 'file1.txt')
         localpath = os.path.join(localtemp, 'file1.txt')
-        rsync.sync_file(localpath, remotepath)
+        self.rsync.sync_file(localpath, remotepath)
 
         with open(localpath, 'r') as copied:
             self.assertEquals('testdata\n', copied.read())
-
