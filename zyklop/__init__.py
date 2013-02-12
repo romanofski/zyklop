78ae3346e2938ff6b639d04ffd83805fe98c7c95
Roman Joost <roman@bromeco.de>
2013-02-12 10:08:57 +1000

    0.5.1 release

--- zyklop/__init__.py
+++ zyklop/__init__.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 __name__ = 'zyklop'
-__version__ = '0.5'
+__version__ = '0.5.1'
 __author__ = 'Róman Joost'
 __author_email__ = 'roman@bromeco.de'
 __description__ = 'Rsync wrapper making syncing easy'
